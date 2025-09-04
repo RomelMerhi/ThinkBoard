f62d9d2ec95eefa678bf9ac38d069cbb4781cdc1
RomelMerhi <r.merhi20@gmail.com>
2025-09-05 00:42:49 +0300

    modifications for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path"
+import { env } from "process";
 // const express = require("express");
 // if package.json has type:"module"
 
@@ -11,11 +13,25 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173",}));
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({origin: "http://localhost:5173",}));
+}
+
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+
+}
 
 //Connect to database if success then start listening 
 connectDB().then(() => 
